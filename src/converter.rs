71221990c36a41d7b3b75ae1315b416fbf9e802a
Rosyid Haryadi <rosyid_haryadi@protonmail.com>
2026-05-27 13:39:46 +0700

    feat: write to file, as_html in arg options

--- src/converter.rs
+++ src/converter.rs
@@ -1,5 +1,96 @@
-use crate::mdparser::BlockToken;
+use crate::mdparser::{BlockToken, InlineToken};
 
-fn to_html(tokens: &Vec<BlockToken>) {
-    
+pub fn to_html(block_tokens: &Vec<BlockToken>) -> String {
+    let mut html = String::new();
+    let mut is_in_ul = false;
+    for block_token in block_tokens {
+        match block_token {
+            BlockToken::Paragraph(content) => {
+                if is_in_ul { html.push_str("</ul>\n"); }
+                is_in_ul = false;
+
+                html.push_str("<p>");
+                html.push_str(&inline_to_html(&content));
+                html.push_str("</p>\n");
+            }
+            BlockToken::Header{level, content} => {
+                if is_in_ul { html.push_str("</ul>\n"); }
+                is_in_ul = false;
+
+                html.push_str(format!("<h{}>", level).as_str());
+                html.push_str(&inline_to_html(&content));
+                html.push_str(format!("</h{}>\n", level).as_str());
+            }
+            BlockToken::Code(content) => {
+                if is_in_ul { html.push_str("</ul>\n"); }
+                is_in_ul = false;
+
+                html.push_str("<pre><code>");
+                html.push_str(&content);
+                html.push_str("</code></pre>");
+            }
+            BlockToken::Line => {
+                if is_in_ul { html.push_str("</ul>\n"); }
+                is_in_ul = false;
+
+                html.push_str("<hr>");
+            }
+            BlockToken::UnorderedListItem(content) => {
+                if !is_in_ul {
+                    html.push_str("<ul>\n");
+                }
+                html.push_str("<li>");
+                html.push_str(&inline_to_html(&content));
+                html.push_str("</li>\n");
+                is_in_ul = true;
+            }
+        }
+    };
+    html
+}
+
+fn inline_to_html(inline_tokens: &Vec<InlineToken>) -> String {
+    let mut html = String::new();
+    for inline_token in inline_tokens {
+        let mut start_tag = String::new();
+        let mut end_tag = String::new();
+        let mut content = String::new();
+        match inline_token {
+            InlineToken::Bold(inner) => {
+                start_tag = "<strong>".to_string();
+                end_tag = "</strong>".to_string();
+                content = inner.to_string();
+            },
+            InlineToken::Italics(inner) => {
+                start_tag = "<i>".to_string();
+                end_tag = "</i>".to_string();
+                content = inner.to_string();
+            }
+            InlineToken::BoldItalics(inner) => {
+                start_tag = "<strong><i>".to_string();
+                end_tag = "</i></strong>".to_string();
+                content = inner.to_string();
+            }
+            InlineToken::Code(inner) => {
+                start_tag = "<code>".to_string();
+                end_tag = "</code>".to_string();
+                content = inner.to_string();
+            }
+            InlineToken::Link { caption, url } => {
+                start_tag = format!("<a href=\"{}\">", url);
+                end_tag = "</a>".to_string();
+                content = caption.to_string();
+            }
+            InlineToken::Image { caption, url } => {
+                start_tag = format!("<img src=\"{}\">", url);
+                end_tag = "</img>".to_string();
+                content = caption.to_string();
+            }
+            InlineToken::Unformatted(unformatted) => { content = unformatted.to_string() }
+        };
+        html.push_str(&start_tag);
+        html.push_str(&content);
+        html.push_str(&end_tag);
+    };
+    html
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod converter;
 
 use crate::mdparser::MdParser;
 use std::fs;
+use std::io::Error;
 use clap::Parser;
+use crate::converter::to_html;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -14,21 +16,31 @@ struct Args {
     #[arg(short, long, action, default_value_t = true)]
     to_stdout: bool,
 
+    #[arg(short, long, action, default_value_t = true)]
+    as_html: bool,
+
     #[arg(short, long, default_value = "parsed_output.html")]
     output: String,
 }
 
-fn main() {
+fn main() -> Result<(), Error> {
     let args = Args::parse();
-    let file_content = fs::read_to_string(args.input)
+    let input_file = &args.input;
+    let file_content = fs::read_to_string(input_file)
         .expect("File does not exist");
 
     let mut parser = MdParser::default();
     let output_tokens = parser.parse(&file_content);
 
+    let output: String = if args.as_html {
+        to_html(&output_tokens)
+    } else {
+        format!("{:#?}", output_tokens).to_string()
+    };
+
     if args.to_stdout {
-        println!("{:#?}", output_tokens);
+        println!("{}", &output);
     }
 
-
+    fs::write(args.output, &output)
 }
